26fd2541e30c1d3b9110764d89364976d6f45169
lyozaa <leo.lexsana@gmail.com>
2019-05-09 00:24:49 -0500

    FINISHED!

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 from flask import Flask, request, redirect, render_template
-#will i need jinja 2 for blog template to work?  render_template enough?
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1200))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
 @app.route("/")
 def index():
     return redirect("/blog")
 
+@app.route("/blog")
+def blog_index():
+    #if you want to see the index page that displays all the blog posts
+    if not request.args.get("id"):
+        all_posts = Blog.query.all()
+        return render_template("blog.html", title="Blog", posts=all_posts)
+    #if you want to access a specific blog's page
+    else: 
+        blog_id_query = request.args.get("id") #this needs to get the k/v of the post and the id # of the post
+        blog_id = Blog.query.get(blog_id_query)
+        return render_template("single-blog.html", post=blog_id)
+
 @app.route("/newpost", methods = ["POST", "GET"])
+def new_post_index():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
 
-@app.route("/blog") #isn't this the same as index? just render_template of this page in index, right?don't need a seperate handler?
-def index():
-    return render_template("/blog", 
-    #template variable = python variable,
-    #template variable = python variable,
-    #template variable = python variable,
-    #template variable = python variable, error messages
-    )
-    
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
 
-def error_protocols():
-    title_error_msg = ""
-    blog_entry_error_msg = ""
-
-    if title == "":
-        title_error_msg = "Please fill in the title."
-    elif body == "":
-        blog_entry_error_msg = "Please fill in the body."
+        single_blog_page = "/blog?id=" + str(new_blog.id)
+        return redirect(single_blog_page)
+    else:
+        return render_template("newpost.html", title="New Post")
 
 
 
 
-
-
-app.run()
+if __name__ == "__main__":
+    app.run()
