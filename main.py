ea011ec88bcf9143d1eecd1d9967d7f75e0b0801
lyozaa <leo.lexsana@gmail.com>
2019-05-12 15:17:15 -0500

    FINISHED!

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1200))
-    #TODO: line 17 user to username?
     owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __init__(self, title, body, owner):
@@ -21,12 +20,13 @@ class Blog(db.Model):
         self.body = body
         self.owner = owner
 
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
-    blog = db.relationship("Blog", backref="owner")
+    blogs = db.relationship("Blog", backref="owner")
 
     def __init__(self, username, password):
         self.username = username
@@ -36,7 +36,7 @@ class User(db.Model):
 @app.before_request
 def require_login():
     #TODO: delete "login" after / is set up
-    route_whitelist = ["signup", "index", "blog_index", "login"]
+    route_whitelist = ["login","blog_index", "index", "signup"]
     if request.endpoint not in route_whitelist and "username" not in session:
         return redirect("/login")
 
@@ -47,15 +47,18 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
+        if not user:
+            flash("That username doesn't exist.", category="error")
+            return render_template("login.html")
+        if user.password != password:
+            flash("That password is incorrect.", category="error")
+            return render_template("login.html")
+        else:
             session["username"] = username
             flash("Logged In!", category="success")
             return redirect("/newpost")
-        else:
-            flash("User password incorrect, or user does not exist", category="error")
-            return render_template("login.html")
-
-    return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/signup", methods=["POST", "GET"])
@@ -72,7 +75,6 @@ def signup():
         #initialize the error messages
         username_error_msg = ""
         password_error_msg = ""
-        verify_password_error_msg = ""
 
         #username qualify conditions
         if username == "":
@@ -85,33 +87,38 @@ def signup():
             username_error_msg = flash("Username cannot contain a space.", category="error")
         else:
             username_error_msg == ""
-
+       
         #password qualify conditions
         if password == "":
             password_error_msg = flash("Please enter a password.", category="error")
+        if len(password) < 3:
+            password_error_msg = flash("Password must be at least 3 characters.", category="error")
         if password != verify:
             password_error_msg = flash("Passwords do not match.", category="error")
-            verify_password_error_msg = flash("Passwords do not match.", category="error")
         else:
             password_error_msg == ""
-            verify_password_error_msg == ""
-
+     
         #if all fields pass qualifying conditions, check the database to see if user is unique
-        if not username_error_msg and not password_error_msg and not verify_password_error_msg:
+        if username_error_msg == "" and password_error_msg == "":
             existing_username = user = User.query.filter_by(username=username).first()
             if not existing_username:
                 new_user = User(username, password)
                 db.session.add(new_user)
                 db.session.commit()
                 session["username"] = username
+                #TODO: Flash "Welcome to Blogz, {{username}}!"
                 return redirect("/newpost")
             else:
+                username_error_msg = flash("That username is already taken.", category="error")
                 return render_template("signup.html", 
-                    username_error = username_error_msg,
-                    password_error = password_error_msg,
-                    verify_error = verify_password_error_msg)
+                    username_error = username_error_msg)
+        else:
+            return render_template("signup.html", 
+                username_error = username_error_msg,
+                password_error = password_error_msg)
 
     return render_template("signup.html")
+
 
 @app.route("/logout")
 def logout():
@@ -122,20 +129,29 @@ def logout():
 
 @app.route("/")
 def index():
-    return redirect("/blog")
+    users = User.query.all()
+    return render_template("index.html", 
+        users=users)
 
 
 @app.route("/blog")
 def blog_index():
-    #To see the index page that displays all the blog posts
-    if not request.args.get("id"):
-        all_posts = Blog.query.all()
-        return render_template("blog.html", title="Blog", posts=all_posts)
-    #To access a specific blog post's page
-    else: 
-        blog_id_query = request.args.get("id") #this needs to get the k/v of the post and the id # of the post
-        blog_id = Blog.query.get(blog_id_query)
-        return render_template("single-blog.html", post=blog_id)
+    # Use Case 1: User clicks on post title to access a single post's page
+    if request.args.get('id'):
+        post_id = request.args.get('id')
+        post = Blog.query.get(post_id)
+        return render_template("single-post.html", post=post)
+    # Use Case 2: User clicks on username link (from index or end of post) to go to user's blog
+    elif request.args.get('user'):
+        owner_id = request.args.get('user')
+        blogger_name = User.query.get(owner_id)
+        posts = Blog.query.filter_by(owner_id=owner_id).all()
+        return render_template("singleUser.html", user=blogger_name, posts=posts)
+    # Use Case 3: User Uses the <nav> bar to access the /blog page with all post from all users displayed
+    else:
+        posts = Blog.query.all()
+        #TODO: Display written by line after blogs
+        return render_template("blog.html", posts=posts)
 
 
 @app.route("/newpost", methods = ["POST", "GET"])
